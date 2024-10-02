4935c4837d00397881724784535073d22166ac15
JestWeoz <tuannguyenquoc057@gmail.com>
2024-10-02 23:01:29 +0700

    update

---
+++ src/main/java/com/example/project2/builder/BuildingSearchBuilder.java
@@ -0,0 +1,151 @@
+package com.example.project2.builder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BuildingSearchBuilder {
+    private String name, ward, street, districtCode, managerName, managerPhoneNumber;
+    private Integer numberOfBasement;
+    private Long floorArea, rentPriceFrom, rentPriceTo, areaFrom, areaTo, staffId;
+    private List<String> typeCode = new ArrayList<String>();
+
+    private BuildingSearchBuilder(Builder builder) {
+        this.name = builder.name;
+        this.ward = builder.ward;
+        this.street = builder.street;
+        this.districtCode = builder.districtCode;
+        this.managerName = builder.managerName;
+        this.managerPhoneNumber = builder.managerPhoneNumber;
+        this.numberOfBasement = builder.numberOfBasement;
+        this.floorArea = builder.floorArea;
+        this.rentPriceFrom = builder.rentPriceFrom;
+        this.rentPriceTo = builder.rentPriceTo;
+        this.areaFrom = builder.areaFrom;
+        this.areaTo = builder.areaTo;
+        this.staffId = builder.staffId;
+        this.typeCode = builder.typeCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getWard() {
+        return ward;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getDistrictCode() {
+        return districtCode;
+    }
+
+    public String getManagerName() {
+        return managerName;
+    }
+
+    public String getManagerPhoneNumber() {
+        return managerPhoneNumber;
+    }
+
+    public Integer getNumberOfBasement() {
+        return numberOfBasement;
+    }
+
+    public Long getFloorArea() {
+        return floorArea;
+    }
+
+    public Long getRentPriceFrom() {
+        return rentPriceFrom;
+    }
+
+    public Long getRentPriceTo() {
+        return rentPriceTo;
+    }
+
+    public Long getAreaFrom() {
+        return areaFrom;
+    }
+
+    public Long getAreaTo() {
+        return areaTo;
+    }
+
+    public Long getStaffId() {
+        return staffId;
+    }
+
+    public List<String> getTypeCode() {
+        return typeCode;
+    }
+    public static class Builder {
+        private String name, ward, street, districtCode, managerName, managerPhoneNumber;
+        private Integer numberOfBasement;
+        private Long floorArea, rentPriceFrom, rentPriceTo, areaFrom, areaTo, staffId;
+        private List<String> typeCode = new ArrayList<String>();
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+        public Builder setWard(String ward) {
+            this.ward = ward;
+            return this;
+        }
+        public Builder setStreet(String street) {
+            this.street = street;
+            return this;
+        }
+        public Builder setDistrictCode(String districtCode) {
+            this.districtCode = districtCode;
+            return this;
+        }
+        public Builder setManagerName(String managerName) {
+            this.managerName = managerName;
+            return this;
+        }
+        public Builder setManagerPhoneNumber(String managerPhoneNumber) {
+            this.managerPhoneNumber = managerPhoneNumber;
+            return this;
+        }
+        public Builder setNumberOfBasement(Integer numberOfBasement) {
+            this.numberOfBasement = numberOfBasement;
+            return this;
+        }
+        public Builder setFloorArea(Long floorArea) {
+            this.floorArea = floorArea;
+            return this;
+        }
+        public Builder setRentPriceFrom(Long rentPriceFrom) {
+            this.rentPriceFrom = rentPriceFrom;
+            return this;
+        }
+        public Builder setRentPriceTo(Long rentPriceTo) {
+            this.rentPriceTo = rentPriceTo;
+            return this;
+        }
+        public Builder setAreaFrom(Long areaFrom) {
+            this.areaFrom = areaFrom;
+            return this;
+        }
+        public Builder setAreaTo(Long areaTo) {
+            this.areaTo = areaTo;
+            return this;
+        }
+        public Builder setStaffId(Long staffId) {
+            this.staffId = staffId;
+            return this;
+        }
+        public Builder setTypeCode(List<String> typeCode) {
+            this.typeCode = typeCode;
+            return this;
+        }
+        public BuildingSearchBuilder build() {
+            return new BuildingSearchBuilder(this);
+        }
+
+    }
+}
---
+++ src/main/java/com/example/project2/converter/BuildingSearchBuilderConverter.java
@@ -0,0 +1,28 @@
+package com.example.project2.converter;
+
+import com.example.project2.builder.BuildingSearchBuilder;
+import com.example.project2.utils.MapUtil;
+
+import java.util.List;
+import java.util.Map;
+
+public class BuildingSearchBuilderConverter {
+    public BuildingSearchBuilder toBuildingSearchBuilder(Map<String, Object> params, List<String> typeCode) {
+        BuildingSearchBuilder buildingSearchBuilder = new BuildingSearchBuilder.Builder()
+                .setName(MapUtil.getObject(params, "name", String.class))
+                .setFloorArea(MapUtil.getObject(params, "floorarea", Long.class))
+                .setWard(MapUtil.getObject(params, "ward", String.class))
+                .setManagerName(MapUtil.getObject(params, "managername",String.class))
+                .setDistrictCode(MapUtil.getObject(params, "districtcide", String.class))
+                .setNumberOfBasement(MapUtil.getObject(params, "numberofbasement", Integer.class))
+                .setTypeCode(typeCode)
+                .setStaffId(MapUtil.getObject(params, "staffid", Long.class))
+                .setAreaFrom(MapUtil.getObject(params, "areafrom", Long.class))
+                .setAreaTo(MapUtil.getObject(params, "areato", Long.class))
+                .setManagerPhoneNumber(MapUtil.getObject(params, "managerphonenumber", String.class))
+                .setRentPriceFrom(MapUtil.getObject(params, "rentpricefrom", Long.class))
+                .setRentPriceTo(MapUtil.getObject(params, "rentpriceto", Long.class))
+                .build();
+        return buildingSearchBuilder;
+    }
+}
